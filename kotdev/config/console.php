b93c86475d201bbf5d3fc51a35d469a1278c9a12
kot <kot.ezhva@gmail.com>
2016-03-16 18:05:39 +0300

    doesn't write console logs in engine

--- kotdev/config/console.php
+++ kotdev/config/console.php
@@ -3,21 +3,8 @@
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 
-    'preload'=>array('log'),
-
     'components'=>array(
 
         'db' => require(dirname(__FILE__).'/../../protected/config/database.php'),
-
-        'log'=>array(
-            'class'=>'CLogRouter',
-            'routes'=>array(
-                array(
-                    'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning',
-                ),
-            ),
-        ),
-
     ),
 );

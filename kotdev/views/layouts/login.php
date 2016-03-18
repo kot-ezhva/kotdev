6105ede591118690fb6a54eb0f035cec38183c81
kot <kot.ezhva@gmail.com>
2016-03-18 10:16:45 +0300

    add mini-styles to login form

--- kotdev/views/layouts/login.php
+++ kotdev/views/layouts/login.php
@@ -1,3 +1,22 @@
-<div class="col-sm-12">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="ru">
+    <?php //Yii::app()->clientScript->registerCoreScript('bootstrap'); ?>
+    <?php Yii::app()->clientScript->registerCoreScript('bootstrap.material'); ?>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700&subset=latin,cyrillic">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+
+    <title><?= CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+<div class="container">
     <?= $content ?>
 </div>
+<script>
+    $.material.init();
+</script>
+</body>
+</html>
--- kotdev/views/user/login.php
+++ kotdev/views/user/login.php
@@ -1,5 +1,30 @@
-<form action="" method="post">
-    <input type="text" name="username">
-    <input type="password" name="password">
-    <input type="submit" value="Submit">
-</form>
+<div class="col-xs-4 col-xs-offset-4">
+    <form class="form-signin form-vertical" method="post">
+        <h2>Войдите</h2>
+        <div class="form-group label-floating">
+            <label class="control-label">Логин</label>
+            <input type="text" class="form-control" autofocus="" required="" name="username">
+        </div>
+
+        <div class="form-group label-floating">
+            <label class="control-label">Пароль</label>
+            <input type="password" class="form-control" required="" name="password">
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+    </form>
+</div>
+<style>
+    body {
+        padding-top: 100px;
+        padding-bottom: 40px;
+        background-color: #E0E0E0;
+        background: linear-gradient(to top right, #BDBDBD, #E0E0E0);
+        background-attachment: fixed;
+    }
+
+    .form-signin{
+        background: #ffffff;
+        padding: 5px 15px;
+        box-shadow: 0 0 10px 2px rgba(0,0,0,0.25);
+    }
+</style>
